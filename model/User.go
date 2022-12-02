e12b1dac739193c98d15b36658c8167510103a8b
duanqiangwei <duanqwei@163.com>
2022-12-02 09:07:23 +0800

    自定义实体类

--- model/User.go
+++ model/User.go
@@ -1,9 +1,15 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 type User struct {
-	gorm.Model
-	Password string `gorm:"column:password"`
-	Username string `gorm:"column:username"`
+	Id        uint   `gorm:"primarykey"`
+	Password  string `gorm:"column:password"`
+	Username  string `gorm:"column:username"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
 }
